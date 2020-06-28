5aca3713846e5f60b981dfe393466d587f8c13df
ForPK <for_pk@naver.com>
2020-06-29 07:18:11 +0900

    BasicJS

--- basic-js/01/js.js
+++ basic-js/01/js.js
@@ -143,11 +143,13 @@ console.log(Object.keys(youngsoo));
 console.log(Object.values(youngsoo));
 
 /* for in */
+//반복문을 실행해야 하는 대상이 객체일 때 주로 사용 key를 불러온다.
 for (let key in youngsoo) {
   console.log(`${key} : ${youngsoo[key]}`);
 }
 
-/* for of 배열 안에 있는 걸 사용할때 */
+/* for of */
+//반복문을 실행해야 하는 대상이 배열일 때 주로 사용 값을 가져온다.
 let numbers = [1, 2, 3];
 for (let anything of numbers) {
   console.log(anything);
@@ -295,12 +297,83 @@ console.log(find); // {category: "duck", name: "daisy", fm: "female"}
 const keyFm = disney.filter((a) => a.fm === "male");
 console.log(keyFm); // male인 것만 가져와서 배열로 만들어줌
 
-/* splice 기존 배열을 건들이며 배열에서 제거 */
-const anyNum = [11, 22, 33, 44, 55];
+/* slice & splice */
+let anyNum = [11, 22, 33, 44, 55];
+
+// slice 기존 배열을 건들이지 않으면서 제거
+const sliced = anyNum.slice(0, 3); // 0번째부터 3번째 앞까지 제거
+console.log(sliced); // [11, 22, 33] 제거되는 값
+console.log(anyNum); // [11, 22, 33, 44, 55]
+
+// splice 기존 배열을 건들이며 배열에서 제거
 const anyNumIndex = anyNum.indexOf(22);
 console.log(anyNumIndex); // 1
 const spliced = anyNum.splice(anyNumIndex, 2); // 22로 부터 2개 지움
 console.log(spliced); // [22, 33] 제거되는 값
 console.log(anyNum); // [11, 44, 55]
 
-// slice 기존 배열을 건들이지 않으면서 제거
+// shift & unshift 맨 앞
+// shift
+const fucShift = anyNum.shift();
+console.log(fucShift); // 11 맨 앞에 있는 원소 기존에서 꺼냄
+anyNum.shift();
+anyNum.shift();
+console.log(anyNum); // 넣은 만큼 모두 뺌
+
+// unshift
+anyNum = [11, 22, 33, 44, 55];
+anyNum.unshift(0);
+console.log(anyNum); // [0, 11, 22, 33, 44, 55]
+// 맨 앞에 원소 추가해줌
+
+// pop & push 맨 뒤
+//pop
+anyNum = [11, 22, 33, 44, 55];
+const fucPop = anyNum.pop();
+console.log(fucPop); // 55 맨 뒤에 있는 원소 기존에서 꺼냄
+anyNum.pop();
+anyNum.pop();
+anyNum.pop();
+console.log(anyNum); // 11
+
+// push
+anyNum = [11, 22, 33, 44, 55];
+anyNum.push(66);
+console.log(anyNum); // [11, 22, 33, 44, 55, 66]
+
+// concat
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+let fucConcat = arr1.concat(arr2);
+console.log(fucConcat); // [1, 2, 3, 4, 5] 배열을 합쳐줌
+console.log(arr1); // [1, 2, 3]
+console.log(arr2); // [4, 5, 6] 기존 배열 건들이지 않음
+
+// spread 연산자
+fucConcat = [...arr1, ...arr2];
+console.log(fucConcat); // [1, 2, 3, 4, 5] 배열을 합쳐줌
+
+// join
+const fucJoin = [1, 2, 3];
+console.log(fucJoin.join()); // 1,2,3 문자열로 변환, 쉼표로 구분
+console.log(fucJoin.join(" ")); // 1 2 3 ' '을 문자열 사이사이 넣어줌
+console.log(fucJoin.join(" # ")); // 1 # 2 # 3
+
+// [참조] 킹강현 최고
+/* Mutating 변하게 된다. */
+//원본 배열을 변형 시킨다.
+//새로운 배열을 반환하지만 주소 참조가 되어 있어서 원본에 수정이 발생한다.
+
+// - array.splice
+// - array.push, array.unshift > 값을 추가 add
+// - array.pop, array.shift > 값을 제거 remove
+// - array.sort, array.fill, array.reverse > 값을 재가공 replace
+
+/* Non Mutating 변하지 않는다. */
+//원본 배열 변형을 시키지 않는다. 새로운 배열을 반환하기 때문에 변형을 시키지 않는다.
+
+// - array.slice
+// - array.concat, spread ... > 값을 추가 add
+// - array.filter > 값을 제거 remove
+// - array.map, array.reduce > 값을 재가공 replace
