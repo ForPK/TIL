c36d9623faa7a51798cf68bb117e4ffbb1b6a912
ForPK <for_pk@naver.com>
2020-06-28 20:28:42 +0900

    BasicJS 2

--- basic-js/01/js.js
+++ basic-js/01/js.js
@@ -65,7 +65,7 @@ print(ironMan);
 print(captainAmerica);
 
 /* getter함수와 setter 함수 */
-/* get함수는 특정 값 조회할때 */
+/* get함수는 특정 값 조회할때마다 호출 */
 const numbers = {
   a: 1,
   b: 2,
@@ -80,7 +80,7 @@ const numbers = {
 numbers.b = 5;
 console.log(numbers.sum);
 
-/* setter */
+/* setter 특정값을 설정할때마다 */
 const iguana = {
   _name: "수영이",
   get name() {
@@ -96,3 +96,67 @@ const iguana = {
 consolw.log(iguana.name);
 iguana.name = "수영이기지배";
 console.log(iguana.name);
+
+/* 배열1 */
+const a = [{ name: "영수" }, { name: "수영" }];
+
+console.log(a[0].name);
+
+a.push({
+  name: "영수영",
+});
+
+console.log(a[2].name);
+
+/* for */
+const aa = ["영수", "수영", "영수영", "똥꼬"];
+
+console.log(aa.length);
+
+for (let i = 0; i < aa.length; i++) {
+  console.log(aa[i]);
+}
+
+/* while 조건이 좀 더 까다로울때 사용. */
+let i = 0;
+let isFun = false;
+
+while (!isFun) {
+  console.log(i);
+  i++;
+  if (i === 20) {
+    isFun = true;
+  }
+}
+
+/* keys, values */
+const youngsoo = {
+  name: "영수사우루스",
+  age: "13",
+  height: "114cm",
+};
+
+console.log(Object.entries(youngsoo));
+// [Array[2], Array[2], Array[2]]
+// 0: 'name' 1: '영수사우루스'
+// keys, values가 묶음으로 하나씩
+console.log(Object.keys(youngsoo));
+console.log(Object.values(youngsoo));
+
+/* for in */
+for (let key in youngsoo) {
+  console.log(`${key} : ${youngsoo[key]}`);
+}
+
+/* for of 배열 안에 있는 걸 사용할때 */
+let numbers = [1, 2, 3];
+for (let anything of numbers) {
+  console.log(anything);
+}
+
+/* continue, break */
+for (let i = 0; i < 10; i++) {
+  if (i === 4) continue; // 조건만족시 스킵
+  console.log(i);
+  if (i === 7) break;
+}
