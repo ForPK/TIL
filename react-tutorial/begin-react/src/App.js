eea6b362ebc56a5520e84cab87fcf12572b1f33b
ForPK <for_pk@naver.com>
2020-07-06 00:27:39 +0900

    props & children

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,32 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Hello from "./Hello.js";
-import "./App.css";
+import Wrapper from "./wrapper.js";
 
 function App() {
-  const name = "react";
-  const style = {
-    backgroundColor: "black",
-    color: "aqua",
-    fontSize: 24,
-    padding: "1rem",
-  };
   return (
-    <Fragment>
-      {/* 주석은 이렇게 */}
-      <Hello
-      // 이렇게도 주석 가능
-      />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </Fragment>
+    <Wrapper>
+      {/* 이 안의 내용들이 children */}
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
-
-// 태그는 꼭 닫혀야 한다. <input />, <div></div>, <Hello/>
-// 두 개 이상 태그는 감싸져 있어야한다.
-// const name = '이렇게';
-// return <div>jacascript 값 보여줄땐 {name}</div>
-// style은 객체 형태로
-// class가 아닌 className
 
 export default App;
--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Hello() {
-  return <div>안녕하세요</div>;
+function Hello({ color, name }) {
+  return (
+    <div
+      style={{
+        color,
+      }}
+    >
+      안녕하세요 {name}
+    </div>
+  );
 }
+
+// 기본적으로 사용할 값 설정 default props
+Hello.defaultProps = {
+  name: "이름 없음",
+};
 
 export default Hello;
