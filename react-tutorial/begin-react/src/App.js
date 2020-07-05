c6203a7c728344abae39f9b74eaa1fb6a64ee96b
ForPK <for_pk@naver.com>
2020-07-06 06:52:33 +0900

    조건부 렌더링

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Wrapper>
       {/* 이 안의 내용들이 children */}
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial={true} {/* ={true}라는 값 설정빼도 true로 간주 */} />
       <Hello color="pink" />
     </Wrapper>
   );
--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Hello({ color, name }) {
+function Hello({ color, name, isSpecial }) {
   return (
     <div
       style={{
         color,
       }}
     >
+      {/* {isSpecial ? <b>&&</b> : null} 값이 다를때*/}
+      {/*  {null, false, undefined}등 나타나지 않지만 {0}은 0으로 나타남 */}
+      {isSpecial && <b>#$%</b>} {/* 단순히 보여주고 숨기고 할때 */}
       안녕하세요 {name}
     </div>
   );
