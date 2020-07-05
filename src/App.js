20446880461064cd9d55e113d008972ca6851b5d
ForPK <for_pk@naver.com>
2020-07-06 07:07:45 +0900

    counter useState 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
-import Hello from "./Hello.js";
-import Wrapper from "./wrapper.js";
+import Counter from "./Counter.js";
 
 function App() {
-  return (
-    <Wrapper>
-      {/* 이 안의 내용들이 children */}
-      <Hello name="react" color="red" isSpecial={true} {/* ={true}라는 값 설정빼도 true로 간주 */} />
-      <Hello color="pink" />
-    </Wrapper>
-  );
+  return <Counter />;
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
+  // number라는 상태를 만들건데 기본 값은 0으로 설정
+  // const numberState = useState(0);
+  // const number = numberState[0];
+  // const setNumber = numberState[1];
+  const [number, setNumber] = useState(0);
   const onIncrease = () => {
-    console.log("+1");
+    setNumber(number + 1);
   };
 
   const onDecrease = () => {
-    console.log("-1");
+    setNumber(number - 1);
   };
   return (
     <div>
-      <h2>0</h2>
+      <h2>{number}</h2>
       <button onClick={onIncrease}>+ 1</button>
       <button onClick={onDecrease}>- 1</button>
       {/* {} 함수를 넣어주는 것. 호출해주는게 아님 onIncrease() */}
     </div>
   );
 }
+// useState를 사용해서 바뀌는 값을 관리할 수 있고 기본 값 설정 가능
+// useState는 배열을 반응하는데 첫번째는 현재상태, 두번째는 바꾸는 이 상태를 바꾸는 함수
 
 export default Counter;
